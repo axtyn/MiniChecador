9cb0f9e123006e462c15cb97089024067f60a21d
Alexa M <nataly.mx@outlook.com>
2015-08-12 10:17:46 -0500

    Continuando version mejorada1

--- guardaredicionalumno.php
+++ guardaredicionalumno.php
@@ -1,6 +1,7 @@
 <?php 
 	include('config.php');
 	$id         = $_POST['id'];
+	//$foto 	= $_POST['foto'];
 	$nombre	 	= $_POST['nombre'];
 	$apellidos1 = $_POST['apellidos1'];
 	$apellidos2 = $_POST['apellidos2'];
@@ -10,5 +11,4 @@
 	$sql = "UPDATE empleado SET nombre='".$nombre."',apellido_paterno='".$apellidos1."',apellido_materno='".$apellidos2."',correo='".$email."',horario='".$horario."' WHERE id_empleado=".$id;
 	mysql_query($sql,$conexion);
 	echo '<center><img src="imagenes/correcto.gif"/>Informacion Actualizada Correctamente.</center>';
-
 ?>
