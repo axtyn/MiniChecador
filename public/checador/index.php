8acc43f2bef92d36534af426d2bd2da1f49e7a44
Alexa M <nataly.mx@outlook.com>
2015-08-17 09:06:42 -0500

    Mejora a interfaz checador

---
+++ public/checador/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/checador.css">
+</head>
+<body>  
+        <div id="copy">
+                <h1>SYMS QUARK (Beta Version 0.06) </h1>
+                <h2>Desarrollado por Eduardo M.</h2>
+        </div>
+        <form name="form1" method="get" action="checador.php?Id=id";> 
+            <img src="../images/Logotipo-Mini.png" width="80px" alt="ATENEA PHARMA">
+            </br><input name="id" class="name" maxlength="2" autofocus required />
+            </br><input name="submit" class="btn" type="submit" value="PRESS" />
+        </form>
+    </div>
+
+</body>
+</html>
